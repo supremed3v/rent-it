775c251dc0f2cb6a2cbb17bc5bf0e2153a60765e
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-01-11 23:22:59 +0500

    bottom elevation for tabbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,13 +2,15 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Products from "../screens/Products";
 import { AntDesign } from '@expo/vector-icons';
+import { useTheme } from "react-native-paper";
 
 export default function TabNavigation() {
+    const theme = useTheme()
     const Tab = createBottomTabNavigator()
     return (
         <Tab.Navigator screenOptions={({ route }) => {
             return {
-                tabBarIcon: ({ focused, color }) => {
+                tabBarIcon: ({ focused }) => {
                     let iconName
                     if (route.name === 'Home') {
                         iconName = focused ? 'home' : 'home'
@@ -22,11 +24,21 @@ export default function TabNavigation() {
                 },
                 tabBarActiveTintColor: '#fff',
                 tabBarInactiveTintColor: 'gray',
-                tabBarActiveBackgroundColor: '#000',
                 headerShown: false,
                 tabBarStyle: {
-                    backgroundColor: '#000',
-                    borderTopColor: '#000',
+                    backgroundColor: theme.dark ? theme.colors.background : '#fff',
+                    borderTopColor: theme.dark ? theme.colors.background : '#fff',
+                    shadowOpacity: 0,
+                    shadowColor: "#fff",
+                    shadowOffset: {
+                        width: 0,
+                        height: 4,
+                    },
+                    shadowOpacity: 0.30,
+                    shadowRadius: 4.65,
+                    
+                    elevation: 15,
+                    
                 }
             }}}
         >
