9be600d301593a02f18beb2dece9075fc70b88c0
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-01-12 01:06:15 +0500

    working on home screen

--- App.js
+++ App.js
@@ -1,27 +1,22 @@
 import {
   DarkTheme as NavigationDarkTheme,
   DefaultTheme as NavigationDefaultTheme,
-} from '@react-navigation/native';
-import {View } from 'react-native';
-import { adaptNavigationTheme, MD3DarkTheme, Provider as PaperProvider } from 'react-native-paper';
-import { SafeAreaView } from 'react-native-safe-area-context';
-;
-import Home from './src/screens/Home';
-import merge from 'deepmerge';
+} from "@react-navigation/native";
+import { View } from "react-native";
+import {
+  adaptNavigationTheme,
+  MD3DarkTheme,
+  Provider as PaperProvider,
+} from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Home from "./src/screens/Home";
+import merge from "deepmerge";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import TabNavigation from './src/components/Navigation';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import TabNavigation from "./src/components/Navigation";
+import { StatusBar } from "expo-status-bar";
 
-// const theme = {
-//   ...DefaultTheme,
-//   dark: true,
-//   colors:{
-//     ...DefaultTheme.colors,
-//     primary: '#e7e7e7e7',
-//     text: '#fff',
-//   }
-// }
 const { LightTheme, DarkTheme } = adaptNavigationTheme({
   reactNavigationLight: NavigationDefaultTheme,
   reactNavigationDark: NavigationDarkTheme,
@@ -36,16 +31,14 @@ const CombinedDarkTheme = {
   },
 };
 
-
-
 export default function App() {
   return (
-    <PaperProvider theme={CombinedDarkTheme} >
-      <SafeAreaView/>
-      <NavigationContainer theme={CombinedDarkTheme} >
-        <TabNavigation/>
+    <PaperProvider theme={CombinedDarkTheme}>
+      <SafeAreaView />
+      <StatusBar style="auto" />
+      <NavigationContainer theme={CombinedDarkTheme}>
+        <TabNavigation />
       </NavigationContainer>
     </PaperProvider>
   );
 }
-
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,170 @@
-import { View } from 'react-native'
-import React from 'react'
-import { useTheme, Text } from 'react-native-paper'
+import { View, ScrollView, Image, FlatList, Dimensions } from "react-native";
+import React from "react";
+import { useTheme, Text, Divider } from "react-native-paper";
 
 export default function Home() {
-    const theme = useTheme()
+  const theme = useTheme();
+
+  const data = [
+    {
+      id: "1",
+    },
+    {
+      id: "2",
+    },
+    {
+      id: "3",
+    },
+    {
+      id: "4",
+    },
+    {
+      id: "5",
+    },
+    {
+      id: "6",
+    },
+  ];
+  const renderItem = ({ item }) => {
+    return (
+      <View
+        style={{
+          width: Dimensions.get("window").width / 2,
+          height: 200,
+          backgroundColor: "#e7e7e7e7",
+          borderRadius: 20,
+          justifyContent: "center",
+          alignItems: "center",
+          flex: 1,
+          margin: 10,
+        }}
+      >
+        <Text
+          style={{
+            color: "#000",
+          }}
+        >
+          {item.id}
+        </Text>
+      </View>
+    );
+  };
   return (
     <View>
-      <Text variant="displayMedium">Home</Text>
+      <ScrollView
+        style={{
+          backgroundColor: theme.dark ? theme.colors.background : "#fff",
+          paddingLeft: 15,
+          paddingRight: 15,
+          marginTop: 20,
+        }}
+      >
+        <View>
+          <Text
+            variant="displayMedium"
+            style={{
+              width: "100%",
+              position: "absolute",
+              top: 100,
+              zIndex: 1,
+              color: "#000",
+              backgroundColor: "#e7e7e7e7",
+              opacity: 0.7,
+              textAlign: "center",
+            }}
+          >
+            Rent everything you need!
+          </Text>
+        </View>
+        <Image
+          source={{
+            uri: "https://images.unsplash.com/photo-1483181957632-8bda974cbc91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+          }}
+          resizeMode="cover"
+          style={{
+            width: "100%",
+            height: 200,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            borderBottomLeftRadius: 20,
+            borderBottomRightRadius: 20,
+          }}
+        />
+        <Divider
+          style={{
+            marginTop: 20,
+            marginBottom: 20,
+            height: 2,
+            backgroundColor: "#fff",
+          }}
+          bold={true}
+          leftInset={true}
+          horizontalInset={true}
+        />
+        <View>
+          <Text variant="displayMedium">Categories</Text>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginTop: 10,
+              }}
+            >
+              <Text
+                variant="displaySmall"
+                style={{
+                  marginRight: 10,
+                }}
+              >
+                Category 1
+              </Text>
+              <Text
+                variant="displaySmall"
+                style={{
+                  marginRight: 10,
+                }}
+              >
+                Category 2
+              </Text>
+              <Text
+                variant="displaySmall"
+                style={{
+                  marginRight: 10,
+                }}
+              >
+                Category 3
+              </Text>
+              <Text
+                variant="displaySmall"
+                style={{
+                  marginRight: 10,
+                }}
+              >
+                Category 4
+              </Text>
+            </View>
+          </ScrollView>
+          <Divider
+            style={{
+              marginTop: 20,
+              marginBottom: 20,
+              height: 2,
+              backgroundColor: "#fff",
+            }}
+            bold={true}
+            leftInset={true}
+            horizontalInset={true}
+          />
+          <Text variant="displayMedium">Products</Text>
+          <FlatList
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={(item, index) => index.toString()}
+            numColumns={2}
+          />
+        </View>
+      </ScrollView>
     </View>
-  )
+  );
 }
