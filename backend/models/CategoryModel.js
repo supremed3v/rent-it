4d2b284c671a2a3d3f939ece25bd922b1f6c15b5
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-01-14 20:23:51 +0500

    added models

---
+++ backend/models/CategoryModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const CategorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.models.Category ||
+  mongoose.model("Category", CategorySchema);
