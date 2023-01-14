a9c6b6efee6d1a854361c57b697b32a9f08545d6
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-01-14 17:55:33 +0500

    completed cart screen

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -1,11 +1,12 @@
-import { View } from "react-native";
+import { View, Image } from "react-native";
 import React from "react";
-import { Text } from "react-native-paper";
+import { Button, Text } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useCart } from "../context/CartContext";
+import { Entypo } from "@expo/vector-icons";
 
 export default function Cart() {
-  const { cart } = useCart();
+  const { cart, removeFromCart } = useCart();
   return (
     <>
       <SafeAreaView />
@@ -13,10 +14,60 @@ export default function Cart() {
         <Text>Cart</Text>
         {cart.length !== 0 ? (
           cart.map((item) => (
-            <View key={item.id}>
-              <Text>{item.name}</Text>
-              <Text>{item.price}</Text>
-              <Text>{item.quantity}</Text>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                marginTop: 20,
+                marginLeft: 20,
+                marginRight: 20,
+              }}
+              key={item.id}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <Image
+                  source={{ uri: item.image }}
+                  style={{
+                    width: 100,
+                    height: 100,
+                    borderRadius: 20,
+                  }}
+                />
+                <View
+                  style={{
+                    marginLeft: 20,
+                  }}
+                >
+                  <Text
+                    variant="bodyLarge"
+                    style={{
+                      fontSize: 20,
+                      fontWeight: "medium",
+                    }}
+                  >
+                    {item.name}
+                  </Text>
+                  <Text
+                    variant=""
+                    style={{
+                      fontSize: 22,
+                      fontWeight: "bold",
+                      marginBottom: 10,
+                    }}
+                  >
+                    PKR {item.price} /-
+                  </Text>
+                </View>
+              </View>
+              <Button onPress={() => removeFromCart(item)}>
+                <Entypo name="trash" size={24} color="white" />
+              </Button>
             </View>
           ))
         ) : (
