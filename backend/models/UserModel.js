cc9c20d824fd316d602bbb16799101801c16e2b9
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-01-14 20:29:12 +0500

    added user and category models

---
+++ backend/models/UserModel.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  root: {
+    type: Boolean,
+    default: false,
+  },
+  avatar: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  products: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  ],
+  orders: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+    },
+  ],
+  rentedItems: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "RentedItem",
+    },
+  ],
+});
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
