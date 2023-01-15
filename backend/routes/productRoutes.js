b3f9c5ce2a63e42dfbf7b549508c94936deb7291
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-01-15 20:13:45 +0500

    added product route

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import Product from "../models/productModel.js";
+import {
+  isAuthenticatedUser,
+  authorizeRoles,
+} from "../middlewares/authenticate";
+import { newProduct } from "../controllers/productController";
+const router = express.Router();
+
+router.post("/add", isAuthenticatedUser, authorizeRoles("seller"), newProduct);
