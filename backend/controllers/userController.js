9dfa89c708c0fbcf1cb1910c828f4a21662c90b1
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-01-15 17:15:40 +0500

    added login controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,3 +25,40 @@ export const registerUser = async (req, res) => {
 
   sendToken(user, 200, res);
 };
+
+// @desc    Login user
+
+// @route   POST /api/v1/login
+
+// @access  Public
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "Please enter all fields",
+    });
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid Credentials",
+    });
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return res.status(400).json({
+      success: false,
+      message: "Incorrect Password",
+    });
+  }
+
+  sendToken(user, 200, res);
+};
