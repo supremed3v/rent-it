9767ed056d5fbb7372340d0e7f7a0aa00155f451
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-01-18 21:53:19 +0500

    added stripe payment API from backend

---
+++ backend/controllers/paymentController.js
@@ -0,0 +1,23 @@
+import stripe from "stripe";
+
+const myStripe = new stripe("");
+
+export const createPayment = async (req, res) => {
+  try {
+    const paymentIntent = await myStripe.paymentIntents.create({
+      amount: req.body.amount,
+      currency: "PKR",
+    });
+    const clientSecret = paymentIntent.client_secret;
+
+    res.status(200).json({
+      success: true,
+      clientSecret,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      err: error.message,
+    });
+  }
+};
