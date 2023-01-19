492a84be6a51359311754c4136f748116b0e28f6
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-01-20 01:55:29 +0500

    added auth context

---
+++ src/context/AuthContext.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { createContext, useContext, useState, useEffect } from "react";
+import { API } from "../context/ProductsContext";
+
+const AuthContext = createContext();
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+  loading: false,
+  error: null,
+};
+
+export const AuthContextProvider = ({ children }) => {
+  const [authState, setAuthState] = useState(initialState);
+
+  const login = async (userCredentials) => {
+    setAuthState({
+      ...authState,
+      loading: true,
+    });
+    try {
+      const res = await axios.post(`${API}/api/v1/login`, userCredentials);
+      setAuthState({
+        ...authState,
+        loading: false,
+        user: res.data.user,
+        isAuthenticated: true,
+      });
+    } catch (error) {
+      setAuthState({
+        ...authState,
+        loading: false,
+        isAuthenticated: false,
+        error: error.response.data.message,
+      });
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        ...authState,
+        login,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
