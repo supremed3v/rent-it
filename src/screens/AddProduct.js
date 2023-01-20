89747206ee1869fc44cd399473843a169c46906d
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-01-20 19:27:43 +0500

    working on products formfields

--- src/screens/AddProduct.js
+++ src/screens/AddProduct.js
@@ -1,12 +1,54 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import React from "react";
-import { Text } from "react-native-paper";
+import { Button, Text, TextInput } from "react-native-paper";
 import Header from "../components/Header";
 
 export default function AddProduct() {
   return (
     <View>
       <Header title={"Add Product"} />
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          style={styles.textField}
+          mode="outlined"
+          label="Enter product name"
+        />
+        <TextInput
+          style={styles.textField}
+          mode="outlined"
+          label="Enter product price"
+        />
+        <TextInput
+          style={styles.textField}
+          mode="outlined"
+          label="Enter product description"
+        />
+        <Button
+          style={{
+            marginTop: 10,
+            marginBottom: 10,
+          }}
+          mode="contained"
+        >
+          Select Category
+        </Button>
+        <Button mode="contained" icon={"camera"}>
+          Select Images
+        </Button>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  textField: {
+    width: 300,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+});
