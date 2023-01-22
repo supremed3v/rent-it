eb03b39a9ebd6baf3f62c22d5504578c6ae227f3
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-01-22 21:21:54 +0500

    conditional rendering of button

--- src/screens/BecomeSeller.js
+++ src/screens/BecomeSeller.js
@@ -7,34 +7,58 @@ import OtpTextInput from "../components/OtpTextInput";
 export default function BecomeSeller() {
   const [code, setCode] = useState("");
   const [pinReady, setPinReady] = useState(false);
+  const [otpSent, setOtpSent] = useState(false);
   const MAX_CODE_LENGTH = 5;
   return (
     <Pressable onPress={Keyboard.dismiss}>
       <Header title={"Hi Seller!"} />
-      <Text>Generate OTP (check your email)</Text>
-      <OtpTextInput
-        setPinReady={setPinReady}
-        code={code}
-        setCode={setCode}
-        maxLength={MAX_CODE_LENGTH}
-      />
-      <View>
-        <Button
-          mode="contained"
-          // dark={true}
-          style={{
-            width: "60%",
-          }}
-          disabled={!pinReady}
-        >
-          <Text
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 50,
+        }}
+      >
+        <Text variant="bodyLarge">Generate OTP (check your email)</Text>
+        {otpSent === false ? (
+          <Button
             style={{
-              color: !pinReady ? "#fff" : "#000",
+              width: "60%",
+              marginTop: 20,
             }}
+            mode="outlined"
+            onPress={() => setOtpSent(true)}
           >
-            {pinReady ? "Verify" : "Enter OTP"}
-          </Text>
-        </Button>
+            <Text>Generate OTP</Text>
+          </Button>
+        ) : (
+          <>
+            <OtpTextInput
+              setPinReady={setPinReady}
+              code={code}
+              setCode={setCode}
+              maxLength={MAX_CODE_LENGTH}
+            />
+            <View>
+              <Button
+                mode="contained"
+                // dark={true}
+                style={{
+                  width: "60%",
+                }}
+                disabled={!pinReady}
+              >
+                <Text
+                  style={{
+                    color: !pinReady ? "#fff" : "#000",
+                  }}
+                >
+                  {pinReady ? "Verify" : "Enter OTP"}
+                </Text>
+              </Button>
+            </View>
+          </>
+        )}
       </View>
     </Pressable>
   );
