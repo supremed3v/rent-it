e5db7a27124fc0863949676c165fca614783a33e
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-01-23 04:05:39 +0500

    added verification tabs

--- src/screens/ImageUpload.js
+++ src/screens/ImageUpload.js
@@ -5,7 +5,6 @@ import { Button, Text } from "react-native-paper";
 const CameraModule = (props) => {
   const [cameraRef, setCameraRef] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
-
   return (
     <Modal
       animationType="slide"
@@ -18,7 +17,7 @@ const CameraModule = (props) => {
       <Camera
         style={{ flex: 1 }}
         ratio="16:9"
-        flashMode={Camera.Constants.FlashMode.off}
+        flashMode={Camera.Constants.FlashMode.on}
         type={type}
         ref={(ref) => {
           setCameraRef(ref);
@@ -53,12 +52,9 @@ const CameraModule = (props) => {
             <TouchableOpacity
               onPress={async () => {
                 if (cameraRef) {
-                  let photo = await cameraRef.takePictureAsync({
-                    base64: true,
-                  });
+                  let photo = await cameraRef.takePictureAsync();
                   props.setImage(photo);
                   props.setModalVisible();
-                  props.setBase64Image(photo);
                 }
               }}
             >
@@ -109,126 +105,12 @@ const CameraModule = (props) => {
     </Modal>
   );
 };
-
-const CardVerification = (props) => {
-  const [cameraRef, setCameraRef] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
-  return (
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={true}
-      onRequestClose={() => {
-        props.setModalVisible();
-      }}
-    >
-      <Camera
-        style={{ flex: 1 }}
-        ratio="16:9"
-        flashMode={Camera.Constants.FlashMode.off}
-        type={type}
-        ref={(ref) => {
-          setCameraRef(ref);
-        }}
-      >
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: "transparent",
-            justifyContent: "flex-end",
-          }}
-        >
-          <View
-            style={{
-              backgroundColor: "black",
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Button
-              icon="close"
-              style={{ marginLeft: 12 }}
-              mode="outlined"
-              color="white"
-              onPress={() => {
-                props.setModalVisible();
-              }}
-            >
-              Close
-            </Button>
-            <TouchableOpacity
-              onPress={async () => {
-                if (cameraRef) {
-                  let photo = await cameraRef.takePictureAsync({
-                    base64: true,
-                  });
-                  props.setCardImage(photo);
-                  props.setModalVisible();
-                  props.setBase64Card(photo);
-                }
-              }}
-            >
-              <View
-                style={{
-                  borderWidth: 2,
-                  borderRadius: 50,
-                  borderColor: "white",
-                  height: 50,
-                  width: 50,
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  marginBottom: 16,
-                  marginTop: 16,
-                }}
-              >
-                <View
-                  style={{
-                    borderWidth: 2,
-                    borderRadius: 50,
-                    borderColor: "white",
-                    height: 40,
-                    width: 40,
-                    backgroundColor: "white",
-                  }}
-                ></View>
-              </View>
-            </TouchableOpacity>
-            <Button
-              icon="axis-z-rotate-clockwise"
-              style={{ marginRight: 12 }}
-              mode="outlined"
-              color="white"
-              onPress={() => {
-                setType(
-                  type === Camera.Constants.Type.back
-                    ? Camera.Constants.Type.front
-                    : Camera.Constants.Type.back
-                );
-              }}
-            >
-              {type === Camera.Constants.Type.back ? "Front" : "Back "}
-            </Button>
-          </View>
-        </View>
-      </Camera>
-    </Modal>
-  );
-};
-
 export default function IDVerification() {
   const [image, setImage] = useState(null);
   const [cardImage, setCardImage] = useState(null);
   const [camera, setShowCamera] = useState(false);
   const [openSecondCamera, setOpenSecondCamera] = useState(false);
   const [hasPermission, setHasPermission] = useState(null);
-  const [base64Card, setBase64Card] = useState(null);
-  const [base64Image, setBase64Image] = useState(null);
-
-  console.log("base64Card", base64Card);
-  console.log("base64Image", base64Image);
-
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
@@ -273,49 +155,6 @@ export default function IDVerification() {
           showModal={camera}
           setModalVisible={() => setShowCamera(false)}
           setImage={(result) => setImage(result.uri)}
-          setBase64Image={(result) => {
-            setBase64Image(result.base64);
-          }}
-        />
-      )}
-      <View>
-        <Text>Upload your ID Card</Text>
-      </View>
-
-      <View
-        style={{
-          backgroundColor: "#fff",
-          width: 120,
-          height: 120,
-          borderRadius: 100,
-          marginBottom: 8,
-        }}
-      >
-        <Image
-          source={{ uri: cardImage }}
-          style={{ width: 120, height: 120, borderRadius: 100 }}
-        />
-      </View>
-      <Button
-        style={{ width: "30%", marginTop: 16 }}
-        icon="camera"
-        mode="contained"
-        onPress={() => {
-          setOpenSecondCamera(true);
-        }}
-      >
-        Camera
-      </Button>
-      {openSecondCamera && (
-        <CardVerification
-          showModal={openSecondCamera}
-          setModalVisible={() => setOpenSecondCamera(false)}
-          setCardImage={(result) =>
-            setCardImage(result.uri) && setBase64Card(result.base64)
-          }
-          setBase64Card={(result) => {
-            setBase64Card(result.base64);
-          }}
         />
       )}
     </View>
