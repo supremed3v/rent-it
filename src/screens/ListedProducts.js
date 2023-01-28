b61dc8bce5156d32b1a0d2c35379ca96b7142b07
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-01-28 09:35:11 +0500

    working on listed products page

---
+++ src/screens/ListedProducts.js
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import React from "react";
+import { Text } from "react-native-paper";
+
+export default function ListedProducts() {
+  return (
+    <View>
+      <Text>ListedProducts</Text>
+    </View>
+  );
+}
