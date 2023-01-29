00b0ae78535f54cda831f4cf338cab4d18d70836
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-01-29 08:22:05 +0500

    completed search screen

--- src/screens/Products.js
+++ src/screens/Products.js
@@ -1,13 +1,57 @@
-import { View } from "react-native";
-import React from "react";
-import { useTheme, Text } from "react-native-paper";
+import { Pressable, View, Keyboard } from "react-native";
+import { useState } from "react";
+import { IconButton, Text, Searchbar } from "react-native-paper";
 import Header from "../components/Header";
 
 export default function Products() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filters, setSelectedFilters] = useState([]);
   return (
-    <View>
+    <Pressable onPress={Keyboard.dismiss}>
       <Header title="Search Products" />
-      <Text>Search Products</Text>
-    </View>
+      <Pressable
+        style={{
+          marginTop: 20,
+          marginLeft: 20,
+        }}
+        onPress={Keyboard.dismiss}
+      >
+        <View
+          style={{
+            justifyContent: "space-between",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <IconButton
+            mode="contained"
+            icon={"filter"}
+            style={{
+              width: 50,
+              backgroundColor: "#e32f45",
+            }}
+          />
+
+          <IconButton
+            mode="contained"
+            icon={"cancel"}
+            style={{
+              width: 50,
+            }}
+            disabled={filters.length === 0}
+          />
+        </View>
+
+        <Searchbar
+          placeholder="Search Product to Rent"
+          value={searchQuery}
+          onChangeText={(query) => setSearchQuery(query)}
+          style={{
+            marginRight: 20,
+            marginTop: 20,
+          }}
+        />
+      </Pressable>
+    </Pressable>
   );
 }
