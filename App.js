b416fb0ef391e55205519e8f541e6b245fdfd9c8
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-01-30 07:55:10 +0500

    added splash screen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import {
   DarkTheme as NavigationDarkTheme,
   DefaultTheme as NavigationDefaultTheme,
 } from "@react-navigation/native";
+import * as SplashScreen from "expo-splash-screen";
 import {
   adaptNavigationTheme,
   MD3DarkTheme,
@@ -9,6 +10,8 @@ import {
 } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "react-native-gesture-handler";
+import { useState, useEffect, useCallback } from "react";
+import LottieView from "lottie-react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { NativeScreen } from "./src/components/Navigation";
@@ -33,13 +36,58 @@ const CombinedDarkTheme = {
   },
 };
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [appReady, setAppReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.hideAsync();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appReady]);
+
+  if (!appReady) {
+    return null;
+  }
+
   return (
     <>
       <AuthContextProvider>
         <ProductProvider>
           <CartProvider>
             <SafeAreaView mode="light" />
+            <GestureHandlerRootView
+              onLayout={onLayoutRootView}
+              style={{
+                flex: 1,
+              }}
+            >
+              <SafeAreaView mode="light" />
+              <LottieView
+                source={require("./assets/rentit-splash.json")}
+                autoPlay
+                loop
+              />
+            </GestureHandlerRootView>
             <PaperProvider theme={CombinedDarkTheme}>
               <NavigationContainer theme={CombinedDarkTheme}>
                 <NativeScreen />
