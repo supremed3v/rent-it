7c7ab4636c781d741c6142c75c660b9af284d9e2
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-02-09 13:39:51 +0500

    working on user details page

---
+++ admin/src/pages/UserDetails.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect, useCallback } from 'react';
+import { useAuthContext } from '../context/AuthContext';
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import { Box, Button, Grid, TextField, Typography, Switch, FormGroup, FormControlLabel } from "@mui/material"
+
+
+export default function UserDetails() {
+    const { id } = useParams()
+    const [userDetails, setUserDetails] = useState([])
+    const { token } = useAuthContext()
+
+    const getUserDetails = useCallback(async () => {
+        try {
+            const { data } = await axios.get(`http://localhost:5000/api/v1/user-details/${id}`, {
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            })
+            setUserDetails(data.user)
+        } catch (error) {
+            console.log(error)
+        }
+    }, [token, id])
+
+    useEffect(() => {
+        getUserDetails()
+    }
+        , [getUserDetails])
+
+    return (
+        <Box>
+            <Typography variant="h4" sx={{
+                marginBottom: "20px"
+            }}>User Details</Typography>
+        </Box>
+    )
+}
