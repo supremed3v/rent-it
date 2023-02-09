607b41a6ac1001a6980e36c04dab198bf6ddea7b
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-02-09 14:31:04 +0500

    added user details

--- admin/src/pages/UserDetails.jsx
+++ admin/src/pages/UserDetails.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect, useCallback } from 'react';
 import { useAuthContext } from '../context/AuthContext';
 import axios from 'axios'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { Box, Button, Grid, TextField, Typography, Switch, FormGroup, FormControlLabel } from "@mui/material"
+import { DataGrid } from '@mui/x-data-grid';
 
 
 export default function UserDetails() {
+    const navigate = useNavigate()
     const { id } = useParams()
     const [userDetails, setUserDetails] = useState([])
     const [sellerProducts, setSellerProducts] = useState([])
@@ -46,93 +48,122 @@ export default function UserDetails() {
         getRentedItems()
     }, [getRentedItems])
 
-    console.log(sellerProducts)
+    const columns = [
+        { field: '_id', headerName: 'ID', width: 70 },
+        { field: 'name', headerName: 'Name', width: 130 },
+        { field: 'price', headerName: 'Price', width: 130 },
+        { field: 'description', headerName: 'Description', width: 130 },
+        { field: 'category', headerName: 'Category', width: 130 },
+        { field: 'images', headerName: 'Image', width: 130 },
+        { field: "approved", headerName: "Approved", width: 130 },
+        { field: "available", headerName: "Availibility", width: 130 },
+        {
+            field: "action", headerName: "Action", width: 170, renderCell: (params) => {
+                const onEdit = () => {
+                    console.log(params.row._id)
+                    navigate(`/product/${params.row._id}`)
+                }
+
+                return (
+                    <>
+                        <Button onClick={onEdit} variant="outlined" size='small' sx={{
+                            backgroundColor: "#f50057",
+                            marginRight: "10px"
+                        }} >View</Button>
+                    </>
+                )
+            }
+        }
+    ]
+    const rows = sellerProducts?.map((product) => {
+        return {
+            id: product._id,
+            _id: product._id,
+            name: product.name,
+            price: product.price,
+            description: product.description,
+            category: product.category,
+            images: product.images && product.images.map((image) =>
+                image[0]?.url
+            ),
+            approved: product.approved,
+            seller: product.seller,
+            available: product.available
+        }
+    })
 
     return (
-        <Box sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            height: "100vh"
+        <>
+            <Box sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
 
-        }} >
-            <Typography variant="h4" sx={{
-                marginBottom: "20px"
-            }}>User Details</Typography>
-            <Grid container spacing={2}>
-                <Grid item xs={12} md={6}>
-                    <TextField
-                        fullWidth
-                        label="Name"
-                        variant="filled"
-                        value={userDetails?.name}
-                        contentEditable={false}
-                    />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <TextField
-                        fullWidth
-                        label="Email"
-                        variant="filled"
-                        value={userDetails?.email}
-
-                        contentEditable={false}
-                    />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <Typography variant="h6" textAlign={"center"}>
-                        Role: {" "}
-                        {userDetails?.role?.charAt(0).toUpperCase() + userDetails?.role?.slice(1)}</Typography>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <Box sx={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center"
-
-                    }}>
-
-                        <img
-                            src={userDetails?.avatar?.url === "url" && "https://picsum.photos/200/300"}
-                            alt={userDetails?.name}
-                            style={{
-                                width: "100px",
-                                height: "100px",
-                                borderRadius: "50%",
-                                objectFit: "cover"
-                            }}
-
+            }} >
+                <Typography variant="h4" sx={{
+                    marginBottom: "20px"
+                }}>User Details</Typography>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={6}>
+                        <TextField
+                            fullWidth
+                            label="Name"
+                            variant="filled"
+                            value={userDetails?.name}
+                            contentEditable={false}
                         />
-                    </Box>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <TextField
+                            fullWidth
+                            label="Email"
+                            variant="filled"
+                            value={userDetails?.email}
+
+                            contentEditable={false}
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Typography variant="h6" textAlign={"center"}>
+                            Role: {" "}
+                            {userDetails?.role?.charAt(0).toUpperCase() + userDetails?.role?.slice(1)}</Typography>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Box sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center"
+
+                        }}>
+
+                            <img
+                                src={userDetails?.avatar?.url === "url" && "https://picsum.photos/200/300"}
+                                alt={userDetails?.name}
+                                style={{
+                                    width: "100px",
+                                    height: "100px",
+                                    borderRadius: "50%",
+                                    objectFit: "cover"
+                                }}
+
+                            />
+                        </Box>
+                    </Grid>
                 </Grid>
-            </Grid>
-            {sellerProducts?.length > 0 && <Typography variant="h4" sx={{
-                marginTop: "20px"
-            }}>Rented Items</Typography>}
-            {sellerProducts?.map((product) => (
-                <Box sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
+            </Box>
+            {sellerProducts?.length > 0 && (
+                <div style={{
+                    height: 400,
+                    width: '100%',
                     marginTop: "20px"
                 }}>
-                    <Typography variant="h6" sx={{
-                        marginBottom: "20px"
-                    }}>Product Name: {product?.name}</Typography>
-                    <img
-                        src={product?.images[0]?.url !== "url" ? product?.images[0]?.url : "https://picsum.photos/200/300"}
-                        alt={product?.name}
-                        style={{
-                            width: "100px",
-                            height: "100px",
-                            borderRadius: "50%",
-                            objectFit: "cover"
-                        }}
+                    <DataGrid
+                        rows={rows}
+                        columns={columns}
+                        pageSize={5}
                     />
-                </Box>
-            ))
-
-            }
-        </Box>
+                </div>
+            )}
+        </>
     )
 }
