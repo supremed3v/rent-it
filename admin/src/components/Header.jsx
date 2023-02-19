90a76d1a28f95b7b29b6faef28e803c78ff92c7c
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-02-19 23:13:39 +0500

    added mailing feature for admin

--- admin/src/components/Header.jsx
+++ admin/src/components/Header.jsx
@@ -1,90 +1,103 @@
-import React from 'react'
-import { useTheme, AppBar, Tooltip, Box, Toolbar, Typography, MenuItem, Menu, Badge, Drawer, Button, IconButton, } from '@mui/material';
-import { NavLink, Outlet } from 'react-router-dom';
+import React from "react";
+import {
+  useTheme,
+  AppBar,
+  Tooltip,
+  Box,
+  Toolbar,
+  Typography,
+  MenuItem,
+  Menu,
+  Badge,
+  Drawer,
+  Button,
+  IconButton,
+} from "@mui/material";
+import { NavLink, Outlet } from "react-router-dom";
 
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import MoreIcon from '@mui/icons-material/MoreVert';
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
 const Header = () => {
-    const theme = useTheme()
-    const [drawerOpen, setDrawerOpen] = React.useState(true)
+  const theme = useTheme();
+  const [drawerOpen, setDrawerOpen] = React.useState(true);
 
-    const toggleDrawer = () => {
-        setDrawerOpen(!drawerOpen)
-    }
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
 
-    let activeStyle = {
-        color: "#333",
-        backgroundColor: "#f5f5f5",
-        textDecoration: "none",
-        padding: "6px 16px",
-        borderRadius: "4px",
-    }
+  let activeStyle = {
+    color: "#333",
+    backgroundColor: "#f5f5f5",
+    textDecoration: "none",
+    padding: "6px 16px",
+    borderRadius: "4px",
+  };
 
-    let inactiveStyle = {
-        color: theme.palette.text.primary,
-        textDecoration: "none",
-        padding: "6px 16px",
-        borderRadius: "4px",
-    }
+  let inactiveStyle = {
+    color: theme.palette.text.primary,
+    textDecoration: "none",
+    padding: "6px 16px",
+    borderRadius: "4px",
+  };
 
-    const routes = [
-        {
-            to: '/',
-            name: 'Home',
-        },
-        {
-            to: 'dashboard',
-            name: 'Dashboard',
-        },
-        {
-            to: 'users',
-            name: 'Users',
-        },
-        {
-            to: 'sellers',
-            name: 'Sellers',
-        },
-        {
-            to: 'products',
-            name: 'Products',
-        },
-        {
-            to: 'orders',
-            name: 'Orders',
-        },
-        {
-            to: 'categories',
-            name: 'Categories',
-        },
-    ]
+  const routes = [
+    {
+      to: "/",
+      name: "Home",
+    },
+    {
+      to: "dashboard",
+      name: "Dashboard",
+    },
+    {
+      to: "users",
+      name: "Users",
+    },
+    {
+      to: "sellers",
+      name: "Sellers",
+    },
+    {
+      to: "products",
+      name: "Products",
+    },
+    {
+      to: "orders",
+      name: "Orders",
+    },
+    {
+      to: "categories",
+      name: "Categories",
+    },
+  ];
 
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="open drawer"
-                        sx={{ mr: 2 }}
-                        onClick={toggleDrawer}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{ display: { xs: 'none', sm: 'block' } }}
-                    >
-                        MUI
-                    </Typography>
-                    {/* <Search>
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="sticky">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+            onClick={toggleDrawer}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
+            MUI
+          </Typography>
+          {/* <Search>
             <SearchIconWrapper>
               <SearchIcon />
             </SearchIconWrapper>
@@ -93,35 +106,39 @@ const Header = () => {
               inputProps={{ 'aria-label': 'search' }}
             />
           </Search> */}
-                    <Box sx={{ flexGrow: 1 }} />
-                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-                            <Badge badgeContent={4} color="error">
-                                <MailIcon />
-                            </Badge>
-                        </IconButton>
-                        <IconButton
-                            size="large"
-                            aria-label="show 17 new notifications"
-                            color="inherit"
-                        >
-                            <Badge badgeContent={17} color="error">
-                                <NotificationsIcon />
-                            </Badge>
-                        </IconButton>
-                        <IconButton
-                            size="large"
-                            edge="end"
-                            aria-label="account of current user"
-                            //   aria-controls={menuId}
-                            aria-haspopup="true"
-                            //   onClick={handleProfileMenuOpen}
-                            color="inherit"
-                        >
-                            <AccountCircle />
-                        </IconButton>
-                    </Box>
-                    {/* <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <IconButton
+              size="large"
+              aria-label="show 4 new mails"
+              color="inherit"
+            >
+              <Badge badgeContent={4} color="error">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={17} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              //   aria-controls={menuId}
+              aria-haspopup="true"
+              //   onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+          {/* <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
             <IconButton
               size="large"
               aria-label="show more"
@@ -133,64 +150,68 @@ const Header = () => {
               <MoreIcon />
             </IconButton>
           </Box> */}
-                </Toolbar>
-
-            </AppBar>
-            <Drawer
-                anchor="left"
-                open={drawerOpen}
-                onClose={toggleDrawer}
-                variant="permanent"
-                style={{
-                    marginTop: '64px',
-                }}
-                sx={{
-
-                    '& .MuiDrawer-paper': {
-                        width: 250,
-                        boxSizing: 'border-box',
-                        marginTop: '64px',
-                    },
-
-                }}
-
-
-            >
-                <Box
-                    sx={{
-                        width: 250,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'space-between',
-                        marginTop: '64px',
-                    }}
-                >
-                    <Box sx={{
-                        flexDirection: 'column',
-                        justifyContent: 'space-between',
-                        display: 'flex',
-                        height: '100%',
-                    }}>
-                        {routes.map((route, index) => (
-                            <NavLink key={index} to={route.to} style={({ isActive }) => isActive ? activeStyle : inactiveStyle}>
-                                <MenuItem >{route.name}</MenuItem>
-                            </NavLink>
-                        ))}
-                    </Box>
-                    <Box>
-                        <MenuItem onClick={toggleDrawer}>Logout</MenuItem>
-                    </Box>
-                </Box>
-            </Drawer>
-            <Box sx={{
-                marginLeft: "260px",
-                marginTop: "64px",
-            }}>
-
-                <Outlet />
-            </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={toggleDrawer}
+        variant="permanent"
+        style={{
+          marginTop: "64px",
+        }}
+        sx={{
+          "& .MuiDrawer-paper": {
+            width: 250,
+            boxSizing: "border-box",
+            marginTop: "64px",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            width: 250,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            marginTop: "64px",
+          }}
+        >
+          <Box
+            sx={{
+              flexDirection: "column",
+              justifyContent: "space-between",
+              display: "flex",
+              height: "100%",
+            }}
+          >
+            {routes.map((route, index) => (
+              <NavLink
+                key={index}
+                to={route.to}
+                style={({ isActive }) =>
+                  isActive ? activeStyle : inactiveStyle
+                }
+              >
+                <MenuItem>{route.name}</MenuItem>
+              </NavLink>
+            ))}
+          </Box>
+          <Box>
+            <MenuItem onClick={toggleDrawer}>Logout</MenuItem>
+          </Box>
         </Box>
-    )
-}
+      </Drawer>
+      <Box
+        sx={{
+          marginLeft: "260px",
+          marginTop: "64px",
+        }}
+      >
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
--- admin/src/pages/SellerDetails.jsx
+++ admin/src/pages/SellerDetails.jsx
@@ -1,16 +1,65 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
-import { Typography, Box, Grid } from "@mui/material";
-
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import {
+  Typography,
+  Box,
+  Grid,
+  Divider,
+  Button,
+  Modal,
+  TextField,
+} from "@mui/material";
+import axios from "axios";
+import { useAuthContext } from "../context/AuthContext";
+function getSellerProducts(token, sellerId) {
+  return axios
+    .get(`http://localhost:5000/api/v1/get-rented-products/${sellerId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((res) => res.data.products) // return the data
+    .catch((err) => console.log(err));
+}
 const SellerDetails = () => {
+  const navigate = useNavigate();
+  const [sellerProducts, setSellerProducts] = useState([]);
+  const { token } = useAuthContext();
   const { state } = useLocation();
   const { seller } = state;
-  console.log(seller);
+  const [open, setOpen] = useState(false);
+  const [email, setEmail] = useState({
+    to: seller.email,
+    subject: "",
+    message: "",
+  });
+
+  useEffect(() => {
+    getSellerProducts(token, seller._id).then((data) => {
+      setSellerProducts(data);
+    });
+  }, [token, seller._id]);
+  console.log(sellerProducts);
   return (
     <div>
-      <Typography variant="h4" sx={{ mb: 2 }}>
-        Seller Details
-      </Typography>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          mb: 2,
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Grid item xs={12} md={6}>
+          <Typography variant="h1">Seller Details</Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Button variant="outlined" onClick={() => setOpen(true)}>
+            Send Message to Seller
+          </Button>
+        </Grid>
+      </Grid>
       <Box sx={{ mb: 2 }}>
         <Grid container spacing={2}>
           <Grid item xs={12} md={6}>
@@ -66,7 +115,98 @@ const SellerDetails = () => {
             <Typography variant="h6">Image not uploaded</Typography>
           )}
         </Grid>
+        <Divider
+          sx={{
+            my: 2,
+          }}
+        />
+        <Box>
+          <Typography variant="h6">Products</Typography>
+          <Grid container spacing={2}>
+            {sellerProducts.length > 0 ? (
+              sellerProducts.map((product) => (
+                <Grid item xs={4} md={4} key={product._id}>
+                  <Box
+                    sx={{
+                      border: "1px solid #ccc",
+                      borderRadius: "5px",
+                      p: 2,
+                      mb: 2,
+                      cursor: "pointer",
+                      "&:hover": {
+                        transform: "scale(1.02)",
+                        boxShadow: "0 0 10px #fff",
+                      },
+                      transition: "all 0.3s ease-in-out",
+                      mt: 2,
+                    }}
+                    onClick={() => navigate(`/product/${product._id}`)}
+                  >
+                    <Typography variant="h6">{product.name}</Typography>
+                    <Typography variant="body1">
+                      {product.description}
+                    </Typography>
+                    <Typography variant="body1">{product.price}</Typography>
+                  </Box>
+                </Grid>
+              ))
+            ) : (
+              <Typography variant="body1">No products</Typography>
+            )}
+          </Grid>
+        </Box>
       </Box>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography variant="h6">Send Email to Seller</Typography>
+          <Divider
+            sx={{
+              my: 2,
+            }}
+          />
+          <TextField
+            label="Subject"
+            variant="outlined"
+            sx={{
+              mb: 2,
+            }}
+            multiline
+            maxRows={4}
+            onChange={(e) => setEmail({ ...email, subject: e.target.value })}
+          />
+          <TextField
+            label="Message"
+            variant="outlined"
+            sx={{
+              mb: 2,
+            }}
+            multiline
+            rows={4}
+            onChange={(e) => setEmail({ ...email, message: e.target.value })}
+          />
+          <Divider />
+          <Button
+            sx={{
+              mt: 2,
+            }}
+            variant="contained"
+          >
+            Send
+          </Button>
+        </Box>
+      </Modal>
     </div>
   );
 };
